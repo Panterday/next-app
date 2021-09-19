d5bb66d73b8c1b0be2d4e213b4bb479a5ccde65c
David <chavezzday@gmail.com>
2021-09-18 23:31:59 -0500

    first

---
+++ pages/products.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+const products = ()=>{
+    return (
+        <div>
+            <h2>PRODUCTS</h2>
+            <Link href="/">
+                <a>Go to home!</a>
+            </Link>
+        </div>
+    )
+}
+
+export default products; 
---
+++ utils/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles({
+    navbar: {
+        backgroundColor: 'red',
+        marginLeft: '1em'
+    }
+})
+
+export default useStyles; 
